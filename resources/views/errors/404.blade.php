aaf276f0f343f4534316687186351264555e51c6
Nyaaan <HunterNyan@users.noreply.github.com>
2015-03-01 01:45:29 +1100

    Revert "Feature/shows"

--- resources/views/errors/404.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('app')
-
-@section('content')
-<div class="full-content text-center" style="border: none;">
-    <h1>404 <span>Page Not Found!</span></h1>
-    <p>Whoops, looks like the page you were looking for doesn't exist!</p>
-</div>
-@endsection
