ded086af4cde05c8715d66b49381fbd45825dd6f
Liam Haworth <liam.haworth@hivemedia.net.au>
2015-03-13 02:37:50 +1100

    #11 - Cleaned up register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,65 +1,43 @@
 @extends('app')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+    <div class="full-content site-register header-center text-center">
+        <h1>User Register</h1>
 
-					<form class="form-horizontal" role="form" method="POST" action="/auth/register">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+        @if(count($errors) > 0)
+            <div class="site-register-errors">
+                <h3 class="register-errors-title">Failed to register new user</h3>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
-							</div>
-						</div>
+                @foreach($errors->all() as $error)
+                    <p class="register-errors-error">{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+        <form class="site-register-form" method="post">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+            <label class="register-form-label" for="name">Nick Name</label>
+            <input type="text" class="register-form-nick" id="name" name="name" value="{{ old('name') }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
+            <br>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+            <label class="register-form-label" for="email">E-Mail Address</label>
+            <input type="email" class="register-form-email" id="email" name="email" value="{{ old('email') }}">
+
+            <br>
+
+            <label class="register-form-label" for="password">Password</label>
+            <input type="password" class="register-form-password" id="password" name="password">
+
+            <br>
+
+            <label class="register-form-label" for="password_conf">Confirm Password</label>
+            <input type="password" class="register-form-password_conf" id="password_conf" name="password_confirmation">
+
+            <br>
+
+            <button type="submit" class="register-form-submit">Register</button>
+        </form>
+    </div>
 @endsection
