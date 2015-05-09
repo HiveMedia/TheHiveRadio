3f2d8f73d43ec7cd45a761c551b026202f733dcd
Liam Haworth <liam.haworth@hivemedia.net.au>
2015-05-10 03:14:18 +1000

    Removed test function from test class that shouldn't have it

--- tests/PlaylistProviderTest.php
+++ tests/PlaylistProviderTest.php
@@ -1,20 +1,6 @@
 <?php
 
 class PlaylistProviderTest extends TestCase {
-
-    /**
-     * Tests route for HTTP 200 OK
-     *
-     * Route: /
-     *
-     * @return void
-     */
-    public function testIndexRoute() {
-        $response = $this->call('GET', '/');
-
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-
-
+    
 
 }
