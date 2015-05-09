cc4803ff073756868106299a4b6921fa62ecc73b
Hunter Nyan <kyle.hunter@hivemedia.net.au>
2015-05-10 03:31:39 +1000

    Add in more Music stuff

--- resources/views/admin/auto/songdb.blade.php
+++ resources/views/admin/auto/songdb.blade.php
@@ -4,6 +4,11 @@
     <div class="full-content site-dashboard">
         <h1 style="text-align: center;">AutoDJ Songs</h1>
         <hr>
-
+        @foreach($Songs as $song)
+            <div>
+                {{ $song['ID'] }} - {{ $song['Title'] }} - {{ $song['Artist'] }} - {{ $song['Album'] }}
+            </div>
+            <hr/>
+        @endforeach
     </div>
 @endsection
